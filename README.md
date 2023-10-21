7db3a751aaf0d55b6817cbac882b6929367da787
Eren <eren.simsek@tuta.io>
2023-10-21 18:26:52 +0100

    polish

---
+++ README.md
@@ -0,0 +1,4 @@
+## subtitler.sh
+Convenience script to extract subtitles from a media file, translate them to the target language, and embed both the original and the target subtitles back into the original media.
+
+Uses [FFMPEG](https://ffmpeg.org) for media handling; [Whisper](https://github.com/ggerganov/whisper.cpp) for speech recognition; and [translatesubs](https://github.com/Montvydas/translatesubs) for translation using Google Translate.
--- subtitler.sh
+++ subtitler.sh
@@ -1,37 +1,63 @@
 #!/bin/bash
 
-sudo pacman -S --needed make git ffmpeg cuda python-pipx
-pipx install translatesubs
-
-filename="$(basename "$1" | sed 's/\(.*\)\..*/\1/')"
-dir="$filename"
-wav_file="${filename}.wav"
-
-mkdir "$dir"
-
-ffmpeg -i "$1" -ar 16000 -ac 1 -c:a pcm_s16le "${dir}/${wav_file}"
-
-if [[ ! -f ./whisper.cpp/main ]]; then
-    git clone https://github.com/ggerganov/whisper.cpp
-    (
-        cd whisper.cpp
-        bash ./models/download-ggml-model.sh large
-
-        if [[ ! -f /opt/cuda/bin/nvcc ]]; then
-            make
-        else
-            WHISPER_CUBLAS=1 make -j
-        fi
-    )
+if [ ! $# -eq 2 ]
+then
+    echo "Input filename and abbreviated target language in that order."
+    exit 0
 fi
 
+sudo pacman -S --needed make git ffmpeg cuda python-pipx &> /dev/null
+pipx install translatesubs &> /dev/null
+
+filepath=$(readlink -f "$1")
+filename="$(basename "$1" | sed 's/\(.*\)\..*/\1/')"
+wav_file="${filename}.wav"
+lang="$2"
+subtitler_dir="${HOME}/.cache/subtitler"
+output_dir="${PWD}/${filename}"
+whisper_dir="${subtitler_dir}/whisper"
+whisper="${whisper_dir}/main"
+model="${whisper_dir}/models/ggml-large.bin"
+
+mkdir -p "$subtitler_dir"
+mkdir -p "$output_dir"
+
+echo "Converting the input file to a usable format..."
+ffmpeg -i "$filepath" -ar 16000 -ac 1 -c:a pcm_s16le "${output_dir}/${wav_file}" -y &> /dev/null
+
+if [[ ! -f "$whisper" ]]; then
+    echo "Downloading Whisper. This may take a while depending on the download speed..."
+    git clone https://github.com/ggerganov/whisper.cpp "$whisper_dir" &> /dev/null
+fi
+
+echo "Setting up Whisper. This may take a while..."
 (
-    cd whisper.cpp
-    ./main -m models/ggml-large.bin -l auto -osrt true -f ../"${dir}/${wav_file}"
+    cd "$whisper_dir"
+    bash ./models/download-ggml-model.sh large &> /dev/null
+
+    if [[ ! -f /opt/cuda/bin/nvcc ]]; then
+        make &> /dev/null
+    else
+        WHISPER_CUBLAS=1 make -j &> /dev/null
+    fi
 )
 
-(
-    cd "$dir"
-    translatesubs "${wav_file}.srt" "${filename}_tr.srt" --to_lang tr --separator " |||  "
-    ffmpeg -i ../"$1" -vf subtitles="${filename}_tr.srt" "${filename}_tr_subs".mp4
-)
+echo "Extracting subtitles from the media file. This will take a while if the media file is large."
+"$whisper" -m  "$model" -l auto -osrt true -f "${output_dir}/${wav_file}" &> /dev/null
+
+echo "Translating subtitles. This will take a while if the media file is large."
+translatesubs "${output_dir}/${wav_file}.srt" "${output_dir}/${filename}_${lang}.srt" --to_lang "$lang" --separator " |||  " &> /dev/null
+
+if ffmpeg -version | grep -F -- --enable-libass &> /dev/null; then
+    echo "Embedding subtitles back into the original media..."
+
+    ffmpeg -i "$filepath" -vf subtitles="${output_dir}/${filename}_${lang}.srt" "${output_dir}/${filename}_${lang}_hardsubs".mp4 -y &> /dev/null
+    ffmpeg -i "$filepath" -i "${output_dir}/${filename}_${lang}.srt" \
+        -i "${output_dir}/${wav_file}.srt" -map 0 -map 1 -map 2 -c copy -c:s mov_text \
+        -metadata:s:s:0 language="$lang" -metadata:s:s:0 language=original \
+        "${output_dir}/${filename}_${lang}_softsubs".mp4 -y &> /dev/null
+else
+    echo "libass is not enabled for FFMPEG. Cannot embed subtitles."
+fi
+
+echo "Done. Results are in ${output_dir}."
